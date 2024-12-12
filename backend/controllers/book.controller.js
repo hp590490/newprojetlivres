d97f4f9b4bbebf23be5d3c966203ad4288e06cb4
Hugo PLASTRE <hugo.plastre@gmail.com>
2024-12-12 10:33:51 +0100

    end

--- backend/controllers/book.controller.js
+++ backend/controllers/book.controller.js
@@ -156,7 +156,11 @@ module.exports.ratingBook = async (req, res) => {
 
     await book.save();
 
-    return res.status(200).json(book);
+    // retourne le livre avec l'image originale car flou avec img resized
+    return res.status(200).json({
+      ...book.toObject(), // transormer en objet JS pour pouvoir modifier les champs, là par ex cest l'imageUrl
+      imageUrl: book.originalImageUrl || book.imageUrl,
+    });
   } catch (error) {
     console.error('Erreur lors de la mise à jour des ratings :', error.message);
     return res.status(500).json({ message: 'Erreur interne du serveur.' });
